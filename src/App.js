fa9169da8c40fa6ad4f01f4bf57f166291c32ed1
yanghao02 <yang>
2020-08-19 16:42:13 +0800

    feat(grow-fe): test for antd theme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import { Button } from 'antd'
+import './App.less'
 
 function App() {
   return (
@@ -18,9 +19,10 @@ function App() {
         >
           Learn React
         </a>
+        <Button type="primary">1111111</Button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
