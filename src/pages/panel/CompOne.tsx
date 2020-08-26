520a2249f4aae6b057e2213be3c57ff4bbcc5173
yanghao02 <yang>
2020-08-26 19:45:39 +0800

    show the render params

--- src/pages/panel/CompOne.tsx
+++ src/pages/panel/CompOne.tsx
@@ -4,8 +4,18 @@ export default (props: any): React.ReactNode => {
   console.log(props)
   return (
     <>
-      <div>This is Panel One!</div>
-      <p>{JSON.stringify(props)}</p>
+      <div>Show the React-Router render params below!</div>
+      <div>
+        {Object.entries(props).map((item) => {
+          const [key, value] = item
+          return (
+            <pre key={key}>
+              <span>{key}: </span>
+              <code>{JSON.stringify(value)}</code>
+            </pre>
+          )
+        })}
+      </div>
     </>
   )
 }
