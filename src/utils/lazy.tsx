caf40b24cee50688206b201d17cb4c9cfc3630d6
Y-lonelY <luckyyoung666@gmail.com>
2020-09-21 16:53:14 +0800

    feat(lazy): add AsyncImport

--- src/utils/lazy.tsx
+++ src/utils/lazy.tsx
@@ -2,19 +2,54 @@ import React, { Suspense } from 'react'
 
 interface importProps {
   action: any
+  loading?: React.ElementType | React.ReactNode
+}
+
+interface AsyncComponentState {
+  component?: React.ElementType | null
 }
 
 export function lazyImport(props: importProps) {
-  const { action } = props
+  const { action, loading } = props
   const LazyComponent = React.lazy(() => action as any)
 
   return (porps: any) => {
     return (
       <div>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={loading || <div>Loading...</div>}>
           <LazyComponent {...porps} />
         </Suspense>
       </div>
     )
   }
+}
+
+export function AsyncImport(props: importProps) {
+  const { action, loading } = props
+  class AsyncComponent extends React.Component<{}, AsyncComponentState> {
+    constructor(props: any) {
+      super(props)
+      this.state = {
+        component: null,
+      }
+    }
+
+    componentDidMount() {
+      action &&
+        action.then((cm: any) => {
+          this.setState({
+            component: cm.default ? cm.default : cm,
+          })
+        })
+    }
+
+    render() {
+      if (this.state.component) {
+        const Current = this.state.component
+        return <Current {...this.props} />
+      }
+      return loading || <div>loading</div>
+    }
+  }
+  return AsyncComponent
 }
